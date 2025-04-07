728bd597019ea3cfd8a1e46a3bd0e2cecd1a0320
Gabriel Miranda <gabriel.miranda@poli.digital>
2025-04-07 17:54:39 -0300

    Initial

---
+++ main.lua
@@ -0,0 +1,7 @@
+#!/usr/bin/lua
+
+local matrix = require("matrices")
+
+matrix:create()
+
+matrix:print()
---
+++ matrices.lua
@@ -0,0 +1,38 @@
+local matrices = {
+  mt = {},
+  N = nil,
+  M = nil
+}
+
+function matrices:create()
+  math.randomseed(os.time())
+
+  self.N = math.random(10)
+  self.M = self.N
+
+  for i=1,self.N do
+    for j=1,self.M do
+      self.mt[i*self.M + j] = math.random(100)
+    end
+  end
+end
+
+function matrices:print()
+  io.write("Printing randomly generated matrix:\n")
+  for i=1,self.N do
+    for j=1,self.M do
+      io.write(self.mt[i*self.M + j], " ")
+    end
+    io.write("\n")
+  end
+end
+
+function matrices:getN()
+  return self.N
+end
+
+function matrices:getM()
+  return self.M
+end
+
+return matrices
